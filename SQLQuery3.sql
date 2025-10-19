0b06e803b26b7e92ced16d936f219dbf463e2371
beknurerzhanov <131442271+beknurerzhanov@users.noreply.github.com>
2025-10-19 19:20:30 +0500

    Refactor column renaming with improved messages

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -2,19 +2,20 @@
 IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='Client' AND COLUMN_NAME='Phone number')
 BEGIN
     EXEC sp_rename 'dbo.Client.[Phone number]', 'PhoneNumber', 'COLUMN';
-    PRINT 'Ïåðåèìåíîâàí Client.[Phone number] -> PhoneNumber';
+    PRINT 'Переименован Client.[Phone number] -> PhoneNumber';
 END
 
 IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='Staff' AND COLUMN_NAME='Date')
 BEGIN
     EXEC sp_rename 'dbo.Staff.[Date]', 'HireDate', 'COLUMN';
-    PRINT 'Ïåðåèìåíîâàí Staff.[Date] -> HireDate';
+    PRINT 'Переименован Staff.[Date] -> HireDate';
 END
 
 IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='RentBook' AND COLUMN_NAME='Time')
 BEGIN
     EXEC sp_rename 'dbo.RentBook.[Time]', 'DurationHours', 'COLUMN';
-    PRINT 'Ïåðåèìåíîâàí RentBook.[Time] -> DurationHours';
+    PRINT 'Переименован RentBook.[Time] -> DurationHours';
 END
 GO
+
 
